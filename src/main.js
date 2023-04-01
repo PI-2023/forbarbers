6a1a6dfbcc6382a280ab27d76010eaf326a88aa7
Isaque Lemos <isaquelemos_bds@hotmail.com>
2023-04-01 08:54:05 -0300

    adicionando vuetify

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import Vuetify from './plugins/vuetify.js';
 
 import './assets/main.css';
 
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount('#app');
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(Vuetify)
+  .mount("#app");
